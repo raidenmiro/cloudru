9f557eb4d3e4f8242dfd704ef9546ea4f08c0977
lordszn3 <robertkuzhin@gmail.com>
2023-06-13 17:30:16 +0500

    fix: fix input component

--- src/shared/view/input/index.tsx
+++ src/shared/view/input/index.tsx
@@ -1,5 +1,6 @@
+import cc from 'classcat'
 import type { ChangeEvent, ComponentPropsWithoutRef, ReactNode } from 'react'
-import { useId } from 'react'
+import { forwardRef, useId } from 'react'
 
 import s from './index.module.css'
 
@@ -7,40 +8,47 @@ type BaseInput = Omit<ComponentPropsWithoutRef<'input'>, 'size' | 'prefix'>
 export interface InputProps extends BaseInput {
   label: string
   onValueChanged?(value: string): void
-  prefix: ReactNode
-  postfix: ReactNode
+  prefix?: ReactNode
+  postfix?: ReactNode
   size?: 'L' | 'M' | 'S'
   variant?: 'primary' | 'secondary'
 }
 
-export const Input = ({
-  label,
-  prefix,
-  postfix,
-  onChange,
-  size = 'L',
-  variant = 'primary',
-  onValueChanged,
-  ...props
-}: InputProps) => {
-  const a11yId = useId()
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      label,
+      prefix,
+      postfix,
+      onChange,
+      size = 'L',
+      variant = 'primary',
+      onValueChanged,
+      className,
+      ...props
+    },
+    ref
+  ) => {
+    const a11yId = useId()
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    onValueChanged?.(e.target.value)
-    onChange?.(e)
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      onValueChanged?.(e.target.value)
+      onChange?.(e)
+    }
+
+    return (
+      <div className={s.field}>
+        <label htmlFor={a11yId}>{label}</label>
+        <input
+          {...props}
+          ref={ref}
+          id={a11yId}
+          className={cc([s.input, className])}
+          data-size={size}
+          data-variant={variant}
+          onChange={handleChange}
+        />
+      </div>
+    )
   }
-
-  return (
-    <div className={s.field}>
-      <label htmlFor={a11yId}>{label}</label>
-      <input
-        {...props}
-        id={a11yId}
-        className={s.input}
-        data-size={size}
-        data-variant={variant}
-        onChange={handleChange}
-      />
-    </div>
-  )
-}
+)
