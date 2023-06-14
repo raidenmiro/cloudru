3296e7f883d88d982a7218ea952649b78b6493ac
lordszn3 <robertkuzhin@gmail.com>
2023-06-14 17:24:18 +0500

    chore: update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,21 @@ const { presets, configure } = require('eslint-kit')
 /** @type {import('eslint-kit').Linter.Config} */
 module.exports = configure({
   presets: [
-    presets.imports({ sort: { newline: true } }),
     presets.typescript(),
     presets.prettier(),
     presets.node(),
     presets.react({ version: '18.0' })
   ],
   extend: {
-    plugins: ['@typescript-eslint'],
+    plugins: ['@typescript-eslint', 'perfectionist'],
     rules: {
-      '@typescript-eslint/consistent-type-imports': 'error'
+      '@typescript-eslint/consistent-type-imports': 'error',
+      'perfectionist/sort-interfaces': 'error'
     }
-  }
+  },
+  extends: [
+    'plugin:perfectionist/recommended-alphabetical',
+    'plugin:perfectionist/recommended-natural',
+    'plugin:perfectionist/recommended-line-length'
+  ]
 })
