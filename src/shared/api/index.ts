47885e03f5778a0346af117edef530cb9b75e7af
lordszn3 <robertkuzhin@gmail.com>
2023-06-17 20:57:55 +0500

    chore: move base url to env and add type response

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,10 +1,15 @@
 import { request } from './adapter'
 
-const BASE_URL = 'https://api.sbercloud.ru/content/v1/bootcamp/frontend'
+const BASE_URL = import.meta.env.VITE_BACKEND_URL
 
-export function sendInformation(form: FormData) {
-  return request({
-    body: form,
+export interface FormAnswer {
+  message: string
+  status: 'success' | 'error'
+}
+
+export function sendForm<P extends Record<string, unknown>>(form: P) {
+  return request<FormAnswer>({
+    body: JSON.stringify(form),
     method: 'POST',
     path: BASE_URL
   })
