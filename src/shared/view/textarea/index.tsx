752a445a46289860f9c4d27f06b07d9268460a87
lordszn3 <robertkuzhin@gmail.com>
2023-06-18 00:32:06 +0500

    feat: add forward ref

--- src/shared/view/textarea/index.tsx
+++ src/shared/view/textarea/index.tsx
@@ -1,8 +1,9 @@
 import cc from 'classcat'
-import type { ComponentPropsWithoutRef } from 'react'
-import { useEffect, useId, useRef, useState } from 'react'
+import type { ChangeEvent, ComponentPropsWithoutRef } from 'react'
+import { forwardRef, useEffect, useId, useRef, useState } from 'react'
 
 import { usePrevious } from '@/shared/lib/hooks/use-previos'
+import { mergeRefs } from '@/shared/lib/merge-refs'
 import { amountOfChars } from '@/shared/lib/string'
 
 import s from './index.module.css'
@@ -16,43 +17,48 @@ export interface TextareaProps extends BaseProps {
   submitOnEnter?: boolean
 }
 
-export const TextArea = ({
-  className,
-  label,
-  maxHeight = '500px',
-  ...props
-}: TextareaProps) => {
-  const [value, setInput] = useState('')
-  const previous = usePrevious(value)
-  const ref = useRef<HTMLTextAreaElement>(null)
+export const TextArea = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  (
+    { className, label, maxHeight = '500px', onChange, value, ...props },
+    forwardedRef
+  ) => {
+    const [input, setInput] = useState((value ?? '') as string)
+    const previous = usePrevious(value)
+    const ref = useRef<HTMLTextAreaElement>(null)
 
-  const a11yId = useId()
-  const count = amountOfChars(value)
+    const a11yId = useId()
+    const count = amountOfChars(input)
 
-  useEffect(() => {
-    const textarea = ref.current
-    if (!textarea) return
+    useEffect(() => {
+      const textarea = ref.current
+      if (!textarea) return
 
-    if (previous !== value) {
-      adjustHeight(textarea, maxHeight)
+      if (previous !== value) {
+        adjustHeight(textarea, maxHeight)
+      }
+    }, [maxHeight, previous, value])
+
+    const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+      setInput(e.target.value)
+      onChange?.(e)
     }
-  }, [maxHeight, previous, value])
 
-  return (
-    <div className={s.paper}>
-      <label htmlFor={a11yId}>{label}</label>
-      <textarea
-        ref={ref}
-        {...props}
-        className={cc([s.textarea, className])}
-        id={a11yId}
-        onChange={(e) => setInput(e.target.value)}
-        value={value}
-      />
-      <div className={s.footer}>
-        <span>Tip</span>
-        <span>{count}</span>
+    return (
+      <div className={s.paper}>
+        <label htmlFor={a11yId}>{label}</label>
+        <textarea
+          ref={mergeRefs(forwardedRef, ref)}
+          {...props}
+          className={cc([s.textarea, className])}
+          id={a11yId}
+          onChange={handleChange}
+          value={value}
+        />
+        <div className={s.footer}>
+          <span>Tip</span>
+          <span>{count}</span>
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
+)
