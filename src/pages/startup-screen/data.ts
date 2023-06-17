73de15434b402b151dce935fb34f7e7d7cbdf96d
lordszn3 <robertkuzhin@gmail.com>
2023-06-17 20:58:53 +0500

    fix: fix resume link

--- src/pages/startup-screen/data.ts
+++ src/pages/startup-screen/data.ts
@@ -1,5 +1,9 @@
 export const data = [
   { id: 1, label: 'Telegram', link: 'https://t.me/raidenmiro' },
   { id: 2, label: 'Github', link: 'https://github.com/raidenmiro' },
-  { id: 3, label: 'Resume', link: 'https://raidenmiro.github.io/resume/' }
+  {
+    id: 3,
+    label: 'Resume',
+    link: 'https://docs.google.com/document/d/1uGUoey0OyHbosCOU7fS9_k9Llr8sZBwggIaij2h6dj0/edit?usp=sharing'
+  }
 ]
