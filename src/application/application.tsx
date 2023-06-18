f0ed59458be5b2fc4051e018291e2fd39301d9a7
lordszn3 <robertkuzhin@gmail.com>
2023-06-19 00:29:46 +0500

    chore: add error boundary

--- src/application/application.tsx
+++ src/application/application.tsx
@@ -1,11 +1,14 @@
 import './application.css'
 
 import { Page } from '@/pages'
+import { ErrorBoundary } from '@/shared/lib/error-boundary'
+
+const MESSAGE = 'Произошла ошибка, перезагрузите страницу'
 
 export function App() {
   return (
-    <>
+    <ErrorBoundary fallback={<div>{MESSAGE}</div>}>
       <Page />
-    </>
+    </ErrorBoundary>
   )
 }
