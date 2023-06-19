7604071168d2cb88266b07a19dc0868cabcf4291
lordszn3 <robertkuzhin@gmail.com>
2023-06-19 21:19:44 +0500

    feat: add validate phone

--- src/pages/startup-screen/schema.ts
+++ src/pages/startup-screen/schema.ts
@@ -1,6 +1,11 @@
 import { object, string } from 'yup'
 
+import { formatTel } from '@/shared/lib/string'
+
 export const startedForm = object({
   email: string().email('Некорректная почта').required('Почта обязательна'),
-  phone: string().required('Номер телефона обязателен')
+  phone: string()
+    .required('Номер телефона обязателен')
+    .transform((v) => formatTel(v))
+    .length(11, 'Некорректный номер телефона')
 })
